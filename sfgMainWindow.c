9d11218dcaf9888b2fc6b0ee08242361eb3e6781
Miguel28021 <miguellomaosorio@gmail.com>
2024-01-04 23:51:32 +0100

    funcion pintar_cuerpo

--- sfgMainWindow.c
+++ sfgMainWindow.c
@@ -13,7 +13,17 @@ struct _SfgMainWindow
   GtkWidget *area;
 };
 
+struct Circulo
+{
+  int masa;
+  int x;
+  int y;
+};
+
 G_DEFINE_TYPE(SfgMainWindow, sfg_main_window, GTK_TYPE_APPLICATION_WINDOW);
+
+static cairo_surface_t *surface = NULL;
+SfgMainWindow *win;
 
 static void
 comenzar_simulacion()
@@ -39,18 +49,71 @@ sfg_main_window_init(SfgMainWindow *win)
   gtk_widget_init_template(GTK_WIDGET(win));
 }
 
-static void draw_cb(GtkDrawingArea *area, cairo_t *cr, int width, int height, gpointer user_data) {
+// borra el contenido de la ventana
+static void
+clear_surface(void)
+{
+  cairo_t *cr;
 
-    double radius = 50.0;
-    double center_x = width / 2.0;
-    double center_y = height / 2.0;
+  cr = cairo_create(surface);
 
-    cairo_set_source_rgb(cr, 0.100, 0.50, 0.32);  // Color negro
-    cairo_set_line_width(cr, 2.0);
+  cairo_set_source_rgb(cr, 1, 1, 1);
+  cairo_paint(cr);
 
-    cairo_arc(cr, center_x, center_y, radius, 0, 2 * G_PI);
-    cairo_fill(cr);
-    }
+  cairo_destroy(cr);
+}
+
+// al modificar tamano de la ventana eliminando el contenido de esta
+static void
+resize_cb(GtkWidget *widget,
+          int width,
+          int height,
+          gpointer data)
+{
+  if (surface)
+  {
+    cairo_surface_destroy(surface);
+    surface = NULL;
+  }
+
+  if (gtk_native_get_surface(gtk_widget_get_native(widget)))
+  {
+    surface = gdk_surface_create_similar_surface(gtk_native_get_surface(gtk_widget_get_native(widget)),
+                                                 CAIRO_CONTENT_COLOR,
+                                                 gtk_widget_get_width(widget),
+                                                 gtk_widget_get_height(widget));
+
+    /* Initialize the surface to white */
+    clear_surface();
+  }
+  printf("RESIZE");
+}
+
+static void pintar_cuerpo()
+{
+  printf("dibujando cuerpo \n");
+  cairo_t *cr = cairo_create(surface);
+
+  // De momento aqui, pero cambiar a futuro
+  struct Circulo miCirculo;
+  
+  miCirculo.masa = 50 ;
+  miCirculo.x = 100;
+  miCirculo.y = 200;
+
+  cairo_set_source_rgb(cr, 0.100, 0.50, 0.32); // Color verde raro -> cambiar
+  cairo_set_line_width(cr, 2.0);
+  cairo_arc(cr, miCirculo.x, miCirculo.y, miCirculo.masa, 0, 2 * G_PI);
+  cairo_fill(cr);
+  cairo_destroy(cr);
+  gtk_widget_queue_draw(win->area);
+}
+
+static void draw_cb(GtkDrawingArea *area, cairo_t *cr, int width, int height, gpointer user_data)
+{
+  cairo_set_source_surface(cr, surface, 0, 0);
+  cairo_paint(cr);
+}
 
 static void
 sfg_main_window_class_init(SfgMainWindowClass *class)
@@ -66,14 +129,15 @@ sfg_main_window_class_init(SfgMainWindowClass *class)
   gtk_widget_class_bind_template_callback(GTK_WIDGET_CLASS(class), comenzar_simulacion);
   gtk_widget_class_bind_template_callback(GTK_WIDGET_CLASS(class), finalizar_simulacion);
   gtk_widget_class_bind_template_callback(GTK_WIDGET_CLASS(class), anadir_cuerpo);
+  gtk_widget_class_bind_template_callback(GTK_WIDGET_CLASS(class), pintar_cuerpo);
+  gtk_widget_class_bind_template_callback(GTK_WIDGET_CLASS(class), resize_cb);
 }
 
 SfgMainWindow *
 sfg_main_window_new(SfgApp *app)
 {
-  SfgMainWindow *win = g_object_new(SFG_MAIN_WINDOW_TYPE, "application", app, NULL);
+  win = g_object_new(SFG_MAIN_WINDOW_TYPE, "application", app, NULL);
   gtk_drawing_area_set_draw_func(GTK_DRAWING_AREA(win->area), draw_cb, NULL, NULL);
-
   return win;
 }
 
