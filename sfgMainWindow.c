a456ee26c35ac58b0386dd61ec02f2febadf6a08
RamonQuerol <128999116+RamonQuerol@users.noreply.github.com>
2024-01-04 17:58:17 +0100

    Añadidos y conectados botones del header de mainWindow

--- sfgMainWindow.c
+++ sfgMainWindow.c
@@ -6,9 +6,28 @@
 struct _SfgMainWindow
 {
   GtkApplicationWindow parent;
+
+  GtkWidget *comenzarButton;
+  GtkWidget *pararButton;
+  GtkWidget *anadirButton;
 };
 
 G_DEFINE_TYPE(SfgMainWindow, sfg_main_window, GTK_TYPE_APPLICATION_WINDOW);
+
+static void
+comenzar_simulacion(){
+  printf("Simulacion comenzada");
+} 
+
+static void
+finalizar_simulacion(){
+  printf("Simulacion finalizada");
+}
+
+static void
+anadir_cuerpo(){
+  printf("Se añadio un cuerpo");
+}
 
 static void
 sfg_main_window_init (SfgMainWindow *win)
@@ -19,8 +38,16 @@ sfg_main_window_init (SfgMainWindow *win)
 static void
 sfg_main_window_class_init (SfgMainWindowClass *class)
 {
-    gtk_widget_class_set_template_from_resource (GTK_WIDGET_CLASS (class),
+  gtk_widget_class_set_template_from_resource (GTK_WIDGET_CLASS (class),
                                                "/com/git/sfg/mainWindow.ui");
+
+  gtk_widget_class_bind_template_child (GTK_WIDGET_CLASS (class), SfgMainWindow, comenzarButton);
+  gtk_widget_class_bind_template_child (GTK_WIDGET_CLASS (class), SfgMainWindow, pararButton);
+  gtk_widget_class_bind_template_child (GTK_WIDGET_CLASS (class), SfgMainWindow, anadirButton);
+
+  gtk_widget_class_bind_template_callback (GTK_WIDGET_CLASS (class), comenzar_simulacion);
+  gtk_widget_class_bind_template_callback (GTK_WIDGET_CLASS (class), finalizar_simulacion);
+  gtk_widget_class_bind_template_callback (GTK_WIDGET_CLASS (class), anadir_cuerpo);
 }
 
 SfgMainWindow *
